7107dc8ad6c71cb635add2327d005452bad77cb1
Mekentosh <m87022928044@gmail.com>
2023-04-08 10:23:18 +0600

    README.md

--- src/Problem2.java
+++ src/Problem2.java
@@ -1,3 +1,25 @@
 import  java.util.Scanner;
 public class Problem2 {
+    public static double arrayAverage(int[] arr, int n) {
+        // Базовый кейс:когда в массиве нет элементов
+        if (n == 0) {
+            return 0;
+        }
+
+        // Рекурсивный кейс: найти сумму элементов с индекса 0 по n-1
+        // разделить на n, чтобы найти среднее значение
+        double sum = arraySum(arr, n-1);
+        return (sum + arr[n-1]) / n;
+    }
+
+    public static int arraySum(int[] arr, int n) {
+        // когда в массиве нет элементов
+        if (n == 0) {
+            return 0;
+        }
+
+        // добавьте текущий элемент к сумме предыдущих элементов
+        return arraySum(arr, n-1) + arr[n-1];
+    }
+
 }
--- src/Problem3.java
+++ src/Problem3.java
@@ -1,3 +1,20 @@
 import  java.util.Scanner;
-public class Problem3 {
+public class Problem3 {public static boolean isPrime(int n) {
+    if (n < 2) {  // числа меньше 2 не являются простыми
+        return false;
+    }
+    return isPrimeHelper(n, 2);
+}
+
+    private static boolean isPrimeHelper(int n, int i) {
+        if (i > Math.sqrt(n)) {  // проверить делимость до квадратного корня из n
+            return true;
+        }
+        if (n % i == 0) {
+            return false;
+        }
+        return isPrimeHelper(n, i + 1);
+    }
+
+
 }

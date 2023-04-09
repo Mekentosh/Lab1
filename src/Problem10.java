e5d4343b99603c757d3d8cbb8c07c2410dfbba40
Mekentosh <m87022928044@gmail.com>
2023-04-09 21:18:23 +0600

    problem 5,6,7,8,9,19

--- src/Problem10.java
+++ src/Problem10.java
@@ -1,3 +1,19 @@
 import  java.util.Scanner;
 public class Problem10 {
+    public static int gcd(int a, int b) {
+        // Base case: if b is 0, the GCD is a
+        if (b == 0) {
+            return a;
+        }
+        // Recursive case: find the GCD of b and the remainder of a divided by b
+        return gcd(b, a % b);
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int gcd = gcd(a, b);
+        System.out.println("GCD(" + a + ", " + b + ") = " + gcd);
+    }
 }

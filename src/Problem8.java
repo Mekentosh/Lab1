fda0660ad252bc76f3366a46c2d91dda47e647a6
Mekentosh <m87022928044@gmail.com>
2023-04-09 21:18:12 +0600

    problem 5,6,7,8,9,19

--- src/Problem8.java
+++ src/Problem8.java
@@ -1,3 +1,16 @@
 import  java.util.Scanner;
 public class Problem8 {
+    public static boolean isAllDigits(String s) {
+        return s.matches("\\d+");
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String s = scanner.nextLine();
+        if (isAllDigits(s)) {
+            System.out.println("The string consists of only digits.");
+        } else {
+            System.out.println("The string does not consist of only digits.");
+        }
+    }
 }

19720c7901e0dabfa6fe228173e94dc7835cecc6
Mekentosh <m87022928044@gmail.com>
2023-04-09 20:19:40 +0600

    problem 4

--- src/Problem4.java
+++ src/Problem4.java
@@ -1,3 +1,16 @@
 import  java.util.Scanner;
 public class Problem4 {
+    public static int factorial(int n) {
+        if (n == 0 || n == 1) {
+            return 1;
+        } else {
+            return factorial(n-1) * n;
+        }
+    }
+
+    public static void main(String[] args) {
+        int n = Integer.parseInt(args[0]);
+        System.out.println(factorial(n));
+    }
+
 }

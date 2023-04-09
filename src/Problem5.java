830865c8f17febc2295a89d6b5d70704c442c234
Mekentosh <m87022928044@gmail.com>
2023-04-09 20:34:56 +0600

    problem 5

--- src/Problem5.java
+++ src/Problem5.java
@@ -1,3 +1,21 @@
-import  java.util.Scanner;
+import java.util.Scanner;
+
 public class Problem5 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int n = input.nextInt();
+        int result = fibonacci(n);
+        System.out.println("The " + n + "th Fibonacci number is " + result);
+    }
+
+    public static int fibonacci(int n) {
+        if (n <= 0) {
+            return 0;
+        } else if (n == 1) {
+            return 1;
+        } else {
+            return fibonacci(n-1) + fibonacci(n-2);
+        }
+    }
 }

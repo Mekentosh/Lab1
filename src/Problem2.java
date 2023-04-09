2910dba1a09243a86c0c644578d5b44d4db2a567
Mekentosh <m87022928044@gmail.com>
2023-04-09 20:36:30 +0600

    problem 5

--- src/Problem2.java
+++ src/Problem2.java
@@ -1,10 +1,9 @@
-import  java.util.Scanner;
+
+
 public class Problem2 {
     public static double arrayAverage(int[] arr, int n) {
         // Базовый кейс:когда в массиве нет элементов
-        if (n == 0) {
-            return 0;
-        }
+        if (n == 0) return 0;
 
         // Рекурсивный кейс: найти сумму элементов с индекса 0 по n-1
         // разделить на n, чтобы найти среднее значение
@@ -22,4 +21,9 @@ public class Problem2 {
         return arraySum(arr, n-1) + arr[n-1];
     }
 
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+        double avg = arrayAverage(arr, arr.length);
+        System.out.println("The average is: " + avg);
+    }
 }
